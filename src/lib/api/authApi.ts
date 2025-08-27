99ace4e132f6607a9f65eca0fb759061b109de3e
Facundo1414 <facu.allende14@gmail.com>
2025-08-27 05:23:12 -0300

    fix: cambio de endpoint

--- src/lib/api/authApi.ts
+++ src/lib/api/authApi.ts
@@ -2,18 +2,18 @@ import { getAccessToken, getRefreshToken } from "@/utils/authToken";
 import api from "./axiosInstance";
 
 export const userLogin = async (email: string, password: string) => {
-  const { data } = await api.post("/auth/login", { email, password });
+  const { data } = await api.post("/api/auth/login", { email, password });
   return data;
 };
 
 export const userLogout = async () => {
   const accessToken = getAccessToken();
-  await api.post("/auth/logout", { accessToken });
+  await api.post("/api/auth/logout", { accessToken });
 };
 
 export const refreshToken = async () => {
   const refreshToken = getRefreshToken();
-  const { data } = await api.post("/auth/refresh", { refreshToken });
+  const { data } = await api.post("/api/auth/refresh", { refreshToken });
   return data.accessToken;
 };
 
@@ -21,7 +21,7 @@ export const checkValidateToken = async (): Promise<boolean> => {
   const token = getAccessToken();
 
   try {
-    const response = await api.post("/auth/validate-token", { accessToken: token });
+  const response = await api.post("/api/auth/validate-token", { accessToken: token });
     return response.status === 200;
   } catch {
     return false;
